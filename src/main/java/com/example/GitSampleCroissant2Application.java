8a80ce4533a13371d27da3f45691f7dce48e5545
CrovArkhangel <SaToShi.KR.1123@gmail.com>
2025-06-05 10:42:58 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleCroissant2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleCroissant2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleCroissant2Application.class, args);
+	}
+
+}
